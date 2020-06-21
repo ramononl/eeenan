e611017c3cb0459d1a66f3e32e107006fa0056f6
ramononl <48051529+ramononl@users.noreply.github.com>
2020-06-21 21:11:12 +0200

    add pages

---
+++ pages/aktivitaeten.vue
@@ -0,0 +1,20 @@
+<template>
+  <PageContainer :title="title">
+    <div>Test</div>
+  </PageContainer>
+</template>
+
+<script>
+import PageContainer from '~/components/layout/PageContainer'
+
+export default {
+  components: {
+    PageContainer
+  },
+  data() {
+    return {
+      title: 'Aktivitäten'
+    }
+  }
+}
+</script>
---
+++ pages/merkliste.vue
@@ -0,0 +1,27 @@
+<template>
+  <PageContainer :title="title">
+    <div class="px-4 uppercase">HTML</div>
+    <div
+      class="mt-1 border-t border-b border-gray-300 divide-y divide-gray-300"
+    >
+      <ListItem />
+    </div>
+  </PageContainer>
+</template>
+
+<script>
+import PageContainer from '~/components/layout/PageContainer'
+import ListItem from '~/components/common/ListItem'
+
+export default {
+  components: {
+    PageContainer,
+    ListItem
+  },
+  data() {
+    return {
+      title: 'Merkliste'
+    }
+  }
+}
+</script>
