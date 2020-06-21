9a368d3201af502442871c4dfd6651d4bce577ca
ramononl <48051529+ramononl@users.noreply.github.com>
2020-06-21 21:11:34 +0200

    fix spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <PageContainer :title="title">
-    <div class="py-4 overflow-x-auto overflow-y-visible scrolling-touch">
+    <div class="pb-4 overflow-x-auto overflow-y-visible scrolling-touch">
       <div class="inline-flex flex-no-wrap px-4 space-x-4">
         <div
           v-for="i in 6"
@@ -11,35 +11,37 @@
         </div>
       </div>
     </div>
-    <div class="p-4 bg-white">
-      <h2 class="text-3xl font-bold">To-Dos</h2>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-    </div>
-    <div class="p-4 bg-white">
-      <h2 class="text-3xl font-bold">To-Dos</h2>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-    </div>
-    <div class="p-4 bg-white">
-      <h2 class="text-3xl font-bold">To-Dos</h2>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-    </div>
-    <div class="p-4 bg-white">
-      <h2 class="text-3xl font-bold">To-Dos</h2>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-    </div>
-    <div class="p-4 bg-white">
-      <h2 class="text-3xl font-bold">To-Dos</h2>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
-      <p>Videotutorials 1–3 ansehen</p>
+    <div class="mt-4 space-y-4">
+      <div class="p-4 bg-white">
+        <h2 class="text-3xl font-bold">To-Dos</h2>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+      </div>
+      <div class="p-4 bg-white">
+        <h2 class="text-3xl font-bold">To-Dos</h2>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+      </div>
+      <div class="p-4 bg-white">
+        <h2 class="text-3xl font-bold">To-Dos</h2>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+      </div>
+      <div class="p-4 bg-white">
+        <h2 class="text-3xl font-bold">To-Dos</h2>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+      </div>
+      <div class="p-4 bg-white">
+        <h2 class="text-3xl font-bold">To-Dos</h2>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+        <p>Videotutorials 1–3 ansehen</p>
+      </div>
     </div>
   </PageContainer>
 </template>
