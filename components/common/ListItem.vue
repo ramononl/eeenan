6f6df8bb0114de4fc1c94c8781e3199272b012bc
ramononl <48051529+ramononl@users.noreply.github.com>
2020-06-21 21:09:40 +0200

    create component

---
+++ components/common/ListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-center justify-between px-4 py-2 space-x-2 bg-white">
+    <div class="flex items-center space-x-2">
+      <svg
+        class="w-10 h-10 mx-auto text-orange-500"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <circle cx="10" cy="10" r="8" />
+      </svg>
+      <p class="text-lg">Kategorie</p>
+    </div>
+    <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+      <path
+        fill-rule="evenodd"
+        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
