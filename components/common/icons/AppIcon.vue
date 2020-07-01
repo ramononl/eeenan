33a13d24d6fb213cd10530127642f92e75bd6b87
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-01 20:14:25 +0200

    add icon components

---
+++ components/common/icons/AppIcon.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    :class="iconClasses"
+  >
+    <keep-alive>
+      <component :is="iconName"></component>
+    </keep-alive>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: false,
+      default: 'X'
+    },
+    button: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    size: {
+      type: Number,
+      required: false,
+      default: 5
+    },
+    color: {
+      type: String,
+      required: false,
+      default: 'text-gray-900'
+    }
+  },
+  computed: {
+    iconName() {
+      return `Icon${this.icon}`
+    },
+    iconClasses() {
+      if (this.insideButton) {
+        return `w-${this.size} h-${this.size} text-${this.color} mr-3 -ml-1`
+      } else {
+        return `w-${this.size} h-${this.size} text-${this.color}`
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg {
+  @apply cursor-pointer;
+  @apply inline-block;
+}
+</style>
