f1d1e9d7da385d281d542fe33c0b68e4671d16a9
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-01 20:15:30 +0200

    add custom height property

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>
+  <div class="overflow-hidden custom-h-screen">
     <nuxt />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.onResize()
+    window.addEventListener('resize', this.onResize)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize)
+  },
+  methods: {
+    onResize(event) {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+  }
+}
 </script>
+
+<style scoped>
+.custom-h-screen {
+  @apply h-screen;
+  height: calc(var(--vh, 1vh) * 100);
+}
+</style>
