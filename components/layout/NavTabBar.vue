7b3a3aa33ad1a80f54fd3e1ed64ddcac15cefddd
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-04 18:16:11 +0200

    change links

--- components/layout/NavTabBar.vue
+++ components/layout/NavTabBar.vue
@@ -4,6 +4,7 @@
   >
     <nuxt-link
       to="/"
+      exact
       class="flex items-center justify-center flex-1 text-xs font-semibold"
       ><span class="flex flex-col items-center"
         ><svg
@@ -22,7 +23,7 @@
       ></nuxt-link
     >
     <nuxt-link
-      to="/kategorien"
+      to="/topics"
       class="flex items-center justify-center flex-1 text-xs font-semibold"
       ><span class="flex flex-col items-center"
         ><svg
@@ -37,7 +38,7 @@
           <path
             d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
           ></path></svg
-        ><span>Kategorien</span></span
+        ><span>Themen</span></span
       ></nuxt-link
     >
     <nuxt-link
