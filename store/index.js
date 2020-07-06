3600cc20e957a9ff64490b830c1fe3cae6fff288
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-06 20:58:19 +0200

    add actions and mutations

--- store/index.js
+++ store/index.js
@@ -2,7 +2,8 @@ export const state = () => ({
   topics: [],
   subtopics: [],
   lessons: [],
-  stories: []
+  stories: [],
+  user: null
 })
 
 export const mutations = {
@@ -11,30 +12,43 @@ export const mutations = {
   },
   setSubtopics(state, payload) {
     state.subtopics = payload
+  },
+  setLessons(state, payload) {
+    state.lessons = payload
+  },
+  setStories(state, payload) {
+    state.stories = payload
+  },
+  setUser(state, payload) {
+    state.user = payload
   }
 }
 
 export const actions = {
-  fetchTopics({ commit }) {
+  async fetchTopics({ commit }) {
     const topics = []
-    this.$fireStore
-      .collection('topics')
-      .orderBy('ordering')
-      .get()
-      .then((res) => {
-        res.forEach((x) => {
-          const id = x.id
-          const data = x.data()
-          topics.push({ id, ...data })
+    try {
+      await this.$fireStore
+        .collection('topics')
+        .orderBy('ordering')
+        .get()
+        .then((res) => {
+          res.forEach((x) => {
+            const id = x.id
+            const data = x.data()
+            topics.push({ id, ...data })
+          })
+          commit('setTopics', topics)
         })
-        commit('setTopics', topics)
-      })
+    } catch (e) {
+      return Promise.reject(e)
+    }
   },
-  fetchSubtopics({ commit }, { subtopic }) {
+  fetchSubtopics({ commit }, { topic }) {
     const subtopics = []
     this.$fireStore
       .collection('topics')
-      .doc(subtopic)
+      .doc(topic)
       .collection('subtopics')
       .orderBy('ordering')
       .get()
@@ -46,5 +60,61 @@ export const actions = {
         })
         commit('setSubtopics', subtopics)
       })
+  },
+  fetchLessons({ commit }, { topic, subtopic }) {
+    const lessons = []
+    this.$fireStore
+      .collection('topics')
+      .doc(topic)
+      .collection('subtopics')
+      .doc(subtopic)
+      .collection('lessons')
+      .orderBy('ordering')
+      .get()
+      .then((res) => {
+        res.forEach((x) => {
+          const id = x.id
+          const data = x.data()
+          lessons.push({ id, ...data })
+        })
+        commit('setLessons', lessons)
+      })
+  },
+  fetchStories({ commit }, { topic, subtopic, lesson }) {
+    const stories = []
+    this.$fireStore
+      .collection('topics')
+      .doc(topic)
+      .collection('subtopics')
+      .doc(subtopic)
+      .collection('lessons')
+      .doc(lesson)
+      .collection('stories')
+      .orderBy('ordering')
+      .get()
+      .then((res) => {
+        res.forEach((x) => {
+          const id = x.id
+          const data = x.data()
+          stories.push({ id, ...data })
+        })
+        commit('setStories', stories)
+      })
+  },
+  async createUser({ commit }, { firstName, lastName, email }) {
+    try {
+      await this.$fireStore
+        .collection('users')
+        .add({
+          firstName,
+          lastName,
+          email
+        })
+        .then(() => {
+          commit('setUser', { firstName, lastName, email })
+        })
+    } catch (e) {
+      return Promise.reject(e)
+    }
   }
 }
