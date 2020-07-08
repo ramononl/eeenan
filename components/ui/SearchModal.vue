d36ab2df850eb9148408cfebaf25d1a11a4eb3f3
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-08 16:26:55 +0200

    change input style

--- components/ui/SearchModal.vue
+++ components/ui/SearchModal.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <label for="search" class="sr-only">Suchen...</label>
     <input
       id="search"
       type="search"
-      class="block w-full px-3 py-2 text-base leading-6 appearance-none rounded-b-md focus:outline-none focus:shadow-outline-orange focus:border-orange-300 focus:z-50"
+      class="block w-full px-3 py-2 text-base leading-10 appearance-none rounded-b-md focus:outline-none"
       placeholder="Suchen..."
     />
   </div>
