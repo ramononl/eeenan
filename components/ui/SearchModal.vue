5f6f75966dfca3b5495dd695c16169abc5fea452
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-09 20:35:10 +0200

    fix line-height/spacing

--- components/ui/SearchModal.vue
+++ components/ui/SearchModal.vue
@@ -3,7 +3,7 @@
     <input
       id="search"
       type="search"
-      class="block w-full px-3 py-2 text-base leading-10 appearance-none rounded-b-md focus:outline-none"
+      class="block w-full px-3 py-4 text-base leading-6 appearance-none rounded-b-md focus:outline-none"
       placeholder="Suchen..."
     />
   </div>
