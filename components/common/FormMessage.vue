1bdf75d34a13e16aca1879560c1f07499aac8b4b
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-09 20:33:52 +0200

    add FormMessage component

---
+++ components/common/FormMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    v-if="message"
+    :class="[
+      message.success ? 'text-green-800 bg-green-50' : 'text-red-800 bg-red-50'
+    ]"
+    class="flex items-center px-3 py-2 mt-4 space-x-2 text-sm font-medium leading-tight rounded-md"
+  >
+    <AppIcon
+      v-if="message.success"
+      :size="6"
+      icon="CheckCircle"
+      color="green-400"
+      class="flex-none"
+    />
+    <AppIcon
+      v-else
+      :size="6"
+      icon="XCircle"
+      color="red-400"
+      class="flex-none"
+    />
+    <span>{{ message.text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
