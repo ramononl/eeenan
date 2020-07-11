f4fea53ef2363ba7bedebe9ab092042717163585
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-11 21:18:51 +0200

    add fetchData plugin

---
+++ plugins/fetchData.js
@@ -0,0 +1,14 @@
+export default ({ store }) => {
+  if (Object.keys(store.state.topics).length === 0) {
+    store.dispatch('fetchTopics')
+  }
+  if (Object.keys(store.state.subtopics).length === 0) {
+    store.dispatch('fetchSubtopics')
+  }
+  if (Object.keys(store.state.lessons).length === 0) {
+    store.dispatch('fetchLessons')
+  }
+  if (Object.keys(store.state.stories).length === 0) {
+    store.dispatch('fetchStories')
+  }
+}
