ffc039261072eb5f135538000df793005765f4e5
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-11 21:28:13 +0200

    check if finished

--- pages/topics/_topic/_subtopic/index.vue
+++ pages/topics/_topic/_subtopic/index.vue
@@ -12,11 +12,13 @@
         class="border-t border-b border-gray-300 divide-y divide-gray-300"
       >
         <ListItem
-          v-for="(lesson, name) in lessons"
-          :key="name"
+          v-for="lesson in lessons"
+          :key="lesson.id"
           :title="lesson.title"
-          :link="name"
+          :link="lesson.id"
+          :finished="finished(lesson.id)"
         />
+        <!-- :finished="true" -->
       </div>
       <div v-else>Loading</div>
     </div>
@@ -24,51 +26,25 @@
 </template>
 
 <script>
-import fetchDataDispatchers from '~/mixins/fetchDataDispatchers'
 import ListItem from '~/components/common/ListItem'
 
 export default {
   components: {
     ListItem
   },
-  mixins: [fetchDataDispatchers],
-  // computed: {
-  //   title() {
-  //     return this.subtopic ? this.subtopic.title : '...'
-  //   }
-  // },
   computed: {
     topicTitle() {
-      if (
-        Object.prototype.hasOwnProperty.call(
-          this.$store.state.topics,
-          this.$route.params.topic
-        )
-      ) {
+      if (Object.keys(this.$store.state.topics).length !== 0) {
         return this.$store.state.topics[this.$route.params.topic].title
       } else {
         return '...'
       }
     },
     title() {
-      if (
-        Object.prototype.hasOwnProperty.call(
-          this.$store.state.subtopics,
-          this.$route.params.topic
-        )
-      ) {
-        if (
-          Object.prototype.hasOwnProperty.call(
-            this.$store.state.subtopics[this.$route.params.topic],
-            this.$route.params.subtopic
-          )
-        ) {
-          return this.$store.state.subtopics[this.$route.params.topic][
-            this.$route.params.subtopic
-          ].title
-        } else {
-          return '...'
-        }
+      if (Object.keys(this.$store.state.subtopics).length !== 0) {
+        return this.$store.state.subtopics[this.$route.params.topic][
+          this.$route.params.subtopic
+        ].title
       } else {
         return '...'
       }
@@ -86,9 +62,14 @@ export default {
             this.$route.params.subtopic
           )
         ) {
-          return this.$store.state.lessons[this.$route.params.topic][
+          const lessons = this.$store.state.lessons[this.$route.params.topic][
             this.$route.params.subtopic
           ]
+          const lessonsArray = Object.keys(lessons).map((key) => {
+            return { id: key, ...lessons[key] }
+          })
+          lessonsArray.sort((a, b) => (a.ordering > b.ordering ? 1 : -1))
+          return lessonsArray
         } else {
           return false
         }
@@ -97,10 +78,21 @@ export default {
       }
     }
   },
-  mounted() {
-    this.fetchTopics()
-    this.fetchSubtopics()
-    this.fetchLessons()
+  methods: {
+    finished(lessonId) {
+      const topic = this.$route.params.topic
+      const subtopic = this.$route.params.subtopic
+      const storiesInLesson = Object.keys(
+        this.$store.state.stories[topic][subtopic][lessonId]
+      )
+      const finishedStories = Object.keys(
+        this.$store.state.user.finishedStories
+      )
+      const allStoriesFinished = storiesInLesson.every((val) =>
+        finishedStories.includes(val)
+      )
+      return allStoriesFinished
+    }
   },
   middleware: 'auth'
 }
