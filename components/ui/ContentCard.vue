78ab8e6dddec387047829f0de17be51aa5e5ca0f
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-15 21:09:15 +0200

    add card component

---
+++ components/ui/ContentCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-4 bg-white border rounded-lg shadow-md">
+    <slot />
+  </div>
+</template>
--- plugins/globalComponents.js
+++ plugins/globalComponents.js
@@ -4,11 +4,13 @@ import AppButton from '~/components/ui/AppButton.vue'
 import AppIcon from '~/components/common/icons/AppIcon.vue'
 import PageContainer from '~/components/layout/PageContainer'
 import MissingContent from '~/components/common/MissingContent'
+import ContentCard from '~/components/ui/ContentCard'
 
 Vue.component('AppButton', AppButton)
 Vue.component('AppIcon', AppIcon)
 Vue.component('PageContainer', PageContainer)
 Vue.component('MissingContent', MissingContent)
+Vue.component('ContentCard', ContentCard)
 
 const ComponentContext = require.context(
   '~/components/common/icons/svg',
