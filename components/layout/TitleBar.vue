78226d51a09a7765bfa471edd7b868645f5259db
ramononl <48051529+ramononl@users.noreply.github.com>
2020-07-16 21:24:15 +0200

    replace icons

--- components/layout/TitleBar.vue
+++ components/layout/TitleBar.vue
@@ -4,7 +4,7 @@
   >
     <div v-if="backButton" class="flex-1">
       <button
-        class="flex items-center focus:outline-none"
+        class="flex items-center -space-x-1 focus:outline-none"
         type="button"
         @click="$router.push(backButton.backLink)"
       >
@@ -22,40 +22,17 @@
     <div v-if="!backButton" class="flex pr-2 space-x-4">
       <button
         type="button"
-        style="focus:outline-none focus:opacity-75"
+        class="focus:outline-none focus:opacity-75"
         @click="openModal('SearchModal')"
       >
-        <svg
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          class="w-8 h-8"
-        >
-          <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-        </svg>
+        <AppIcon :size="9" icon="Search" color="gray-500" />
       </button>
       <button
         type="button"
-        style="focus:outline-none focus:opacity-75"
+        class="focus:outline-none focus:opacity-75"
         @click="openModal('SettingsModal')"
       >
-        <svg
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          class="w-8 h-8"
-        >
-          <path
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-          ></path>
-          <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-        </svg>
+        <AppIcon :size="9" icon="Cog" color="gray-500" />
       </button>
     </div>
   </div>
